85f08a6f3b6c5696b5a94d0c71c1e920f815d424
Ronak Shoghi <ronak.shoghi@ruhr-uni-bochum.de>
2022-05-05 00:09:41 +0200

    From_Michigan

--- src/Main_Script.py
+++ src/Main_Script.py
@@ -15,7 +15,7 @@ import Result_Parser as RP
 #Data_Base = DC.Insert_Intital_Load(load_cases)
 #DC.Json_Database_Creator(Data_Base, "Data_Base_Updated.json")
 #RP.Results_Reader(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]))
-#RP.Results_Writer(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]), "Data_Base_Updated.json")
+RP.Results_Writer(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]), "Data_Base_Updated.json")
 
 # Keys = Data_Base.keys()
 # Desired_Keys = KP.Key_Finder(Keys, [1, 1, 1, 0, 0, 0])
