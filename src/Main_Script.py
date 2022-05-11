19774f231039f251ef7f54e2131e00d511b78f8b
Ronak Shoghi <ronak.shoghi@ruhr-uni-bochum.de>
2022-05-11 16:30:11 +0200

    Meta Data Writer

--- src/Main_Script.py
+++ src/Main_Script.py
@@ -10,12 +10,13 @@ import Key_Parser as KP
 import Key_Generator as KG
 import Database_Creator as DC
 import Result_Parser as RP
+import Meta_reader as MR
 
 #load_cases = 'sigdata1.txt'
 #Data_Base = DC.Insert_Intital_Load(load_cases)
 #DC.Json_Database_Creator(Data_Base, "Data_Base_Updated.json")
-#RP.Results_Reader(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]))
-RP.Results_Writer(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]), "Data_Base_Updated.json")
+MR.Meta_Reader(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]))
+#RP.Results_Writer(KG.Key_Generator([-7.626425197906852, 20.504493245472673, -36.88058941350355, 0.0, 0.0, 0.0]), "Data_Base_Updated.json")
 
 # Keys = Data_Base.keys()
 # Desired_Keys = KP.Key_Finder(Keys, [1, 1, 1, 0, 0, 0])
