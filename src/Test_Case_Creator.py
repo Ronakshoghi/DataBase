5a41f2fe6d1d87cdc4c9d47ca3842ac7c03feba7
Ronakshoghi <94053464+Ronakshoghi@users.noreply.github.com>
2023-03-29 13:42:11 +0200

    Delete src directory

--- src/Test_Case_Creator.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Author: Ronak Shoghi
-Date: 13.09.22
-Time: 09:40
-
-"""
-#A fuction to create elastic and plastic data points for
-# testing tranied machine learning algorithm. For this purpose
