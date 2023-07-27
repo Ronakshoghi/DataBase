29ee62172455ef52a64c7292f069bf75b8bc5faf
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2023-07-27 12:03:50 -0400

    Changed to Python style guide conventions (only lower cae letters for functions). Added OpenPhase if clause to read and write results.

--- src/Result_Parser.py
+++ src/Result_Parser.py
@@ -6,49 +6,95 @@ Time: 02:27
 
 """
 import os
-import Database_Handler as DH
+import warnings
+
+from src import Database_Handler as DH
 import json
 import numpy as np
-import Key_Generator as KG
-import Strain_Result_Check as SRC
-import Meta_reader as MR
+from src import Key_Generator as KG
+from src import Strain_Result_Check as SRC
+from src import Meta_reader as MR
 """
 Thia function read all the output files in the result folder and write them in the database. 
 """
 
-def Results_Reader (Key):
-    Current_Path = os.getcwd()
-    Keys_Path ="{}/Keys".format(Current_Path)
-    os.chdir(Keys_Path)
-    Key_path = os.path.abspath(Key)
-    Results_Path = "{}/results".format(Key_path)
-    os.chdir(Results_Path)
-    Results_Files_Name = []
+def results_reader (key, cp_code='abaqus'):
+    current_path = os.getcwd()
+    keys_path ="{}/Keys".format(current_path)
+    os.chdir(keys_path)
+    key_path = os.path.abspath(key)
+    results_path = "{}/results".format(key_path)
+    os.chdir(results_path)
+    results_files_name = []
     for root, dirs, files in os.walk(os.getcwd(), topdown=False):
         for name in files:
-            Results_Files_Name.append(name)
-    Available_Results = {}
-    for Result_File in Results_Files_Name:
-        with open("{}/{}".format(Results_Path, Result_File)) as Temp_Result:
-            lines = Temp_Result.readlines()
-            Values = []
-            for line in lines:
-                Values.append(float(line.strip('\n')))
-            Available_Results[Result_File.strip(".out")] = Values
-    os.chdir(Current_Path)
-    return Available_Results
+            results_files_name.append(name)
+    available_results = {}
 
-def Results_Writer (key, json_file):
-    Results = Results_Reader(key)
+    for result_file in results_files_name:
+        if cp_code == 'abaqus':
+            with open("{}/{}".format(results_path, result_file)) as Temp_Result:
+                lines = Temp_Result.readlines()
+                values = []
+                for line in lines:
+                    values.append(float(line.strip('\n')))
+                available_results[result_file.strip(".out")] = values
+        elif cp_code == 'openphase':
+            if len(results_files_name) > 1:
+                warnings.warn("WARNING: More then one OpenPhase results.txt found in folder!\n Might be not wanted!")
+            openphase_out = np.genfromtxt(result_file, delimiter=';', skip_header=1)
+            stresses_cauchy = openphase_out[:, 0:6] * 1e-6 # convert to MPa
+            stresses_vm = openphase_out[:, -1] * 1e-6 # convert to MPa
+
+            strains_green = openphase_out[:, 6:12]
+            strains_pl = openphase_out[:, 12:18]
+            strains_vm = openphase_out[:, -2]
+
+
+            # FLIP TO VOIGT
+            stresses_cauchy[:, [3, 4]] = stresses_cauchy[:, [4, 3]]
+            stresses_cauchy[:, [4, 5]] = stresses_cauchy[:, [5, 4]]
+            strains_green[:, [3, 4]] = strains_green[:, [4, 3]]
+            strains_green[:, [4, 5]] = strains_green[:, [5, 4]]
+            strains_pl[:, [3, 4]] = strains_pl[:, [4, 3]]
+            strains_pl[:, [4, 5]] = strains_pl[:, [5, 4]]
+
+
+            # Assign arrays to available results dict
+            keys_stress = ['S11', 'S22', 'S33', 'S32', 'S13', 'S12']
+            keys_strain = ['E11', 'E22', 'E33', 'E32', 'E13', 'E12']
+            keys_strain_pl = ['Ep11', 'Ep22', 'Ep33', 'Ep32', 'Ep13', 'Ep12']
+
+            for idx_component, key_stress in enumerate(keys_stress):
+                available_results[key_stress] = stresses_cauchy[:, idx_component].tolist()
+            for idx_component, key_strain in enumerate(keys_strain):
+                available_results[key_strain] = strains_green[:, idx_component].tolist()
+            for idx_component, key_strain_pl in enumerate(keys_strain_pl):
+                available_results[key_strain_pl] = strains_pl[:, idx_component].tolist()
+
+            available_results['S'] = stresses_vm.tolist()
+            available_results['E'] = strains_vm.tolist()
+
+
+
+        else:
+            raise ValueError("cp_code {code} not valid. Must be abaqus or openphase.".format(code=cp_code))
+
+    os.chdir(current_path)
+    return available_results
+
+#TODO: When is the writer used? In Main_Script.py, only the reader is used to create the results dict
+def results_writer (key, json_file):
+    results = results_reader(key, 'abaqus')
     with open(json_file, 'w') as output_file:
-        Current_Dict = DC.Read_Database_From_Json("Data_Base.json")
-        if key in Current_Dict.keys():
+        current_dict = DH.read_database_from_json("Data_Base.json")
+        if key in current_dict.keys():
             print("The key is already found in JSON file")
-            temp_dic = Current_Dict[key]
-            for result in Results:
-                temp_dic[result] = Results[result]
-            Current_Dict.update(temp_dic)
-            json.dump(Current_Dict, output_file)
+            temp_dic = current_dict[key]
+            for result in results:
+                temp_dic[result] = results[result]
+            current_dict.update(temp_dic)
+            json.dump(current_dict, output_file)
 
         else:
             print("The key was not found in JSON file")
@@ -59,32 +105,32 @@ def Results_Writer (key, json_file):
 # abspath = os.path.abspath(__file__)
 # dname = os.path.dirname(abspath)
 # os.chdir(dname)
-# Results_Dict = DH.Read_Database_From_Json("Data_Base.json")
+# results_dict = DH.Read_Database_From_Json("Data_Base.json")
 # load_cases = "sigdata1.txt"
 # loads = np.genfromtxt(load_cases)
-# Source_Path = os.getcwd()
+# source_path = os.getcwd()
 # os.chdir('..')
-# Current_Path = os.getcwd()
+# current_path = os.getcwd()
 #
 # "Main Process"
 #
 # for counter, load in enumerate(loads):
 #     print (counter)
-#     Key = KG.Key_Generator(load)
-#     if Key in Results_Dict.keys():
+#     key = KG.key_generator(load)
+#     if key in results_dict.keys():
 #         print("The key is already found in JSON file")
 #         continue
 #     else:
 #         print("The key was not found in JSON file")
 #         scaling_factor = 80
 #         scaled_load = load * scaling_factor
-#         Max_Strain = SRC.Max_Strain_Finder(Key)
+#         Max_Strain = SRC.max_strain_finder(key)
 #         #Insert Results to the Data Base
-#         Results_Dict[Key] = {"Meta_Data": MR.Meta_reader(Key),
+#         results_dict[key] = {"Meta_Data": MR.meta_reader(key),
 #                              "Initial_Load": load.tolist(),
 #                              "Scaling_Factor": scaling_factor,
 #                              "Applied_Load": scaled_load.tolist(),
 #                              "Max_Strain": Max_Strain,
-#                              "Results": Results_Reader(Key)}
+#                              "Results": results_reader(key)}
 #
-#     DH.Json_Database_Creator(Results_Dict, "Data_Base_Updated.json")
+#     DH.json_database_creator(results_dict, "Data_Base_Updated.json")
