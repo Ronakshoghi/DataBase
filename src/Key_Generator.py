61f70f370a7162f0068d1d35b80c905f5a59e18f
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2023-07-27 11:56:19 -0400

    Changed to Python style guide conventions (only lower cae letters for functions). Changed key to include information on the RVE size.

--- src/Key_Generator.py
+++ src/Key_Generator.py
@@ -5,7 +5,7 @@ Date: 07.04.22
 Time: 15:22
 
 """
-#It should read sigdata or load case from Abaqus_Temp_files
+# It should read sigdata or load case from Abaqus_Temp_files
 """
 This function creates a unique key for each load case which then will be used to identify and select any desired load case in the database. 
 Symbol Guidline:
@@ -23,30 +23,50 @@ F = sigma 12
 """
 import hashlib
 import os
+import warnings
 
 
-def Key_Generator(Load_Case):
+def key_generator(load_case, n_grains_per_dir=11, elements_per_grain=8, cp_code='abaqus', ori_file='Orientation.txt'):
+    current_path = os.getcwd()
+    if cp_code == 'abaqus':
+        temp_files_path = "{}/Abaqus_Temp_Files".format(current_path)
+    elif cp_code == 'openphase':
+        temp_files_path = "{}/OpenPhase_Temp_Files".format(current_path)
+    else:
+        raise ValueError("cp_code {code} not valid. Must be abaqus or openphase.".format(code=cp_code))
+    orientation_file_path = "{}/{}".format(temp_files_path, ori_file)
+    load_evaluation = []
 
-    Current_Path = os.getcwd()
-    Abaqus_Temp_Files_Path = "{}/Abaqus_Temp_Files".format(Current_Path)
-    Orientation_File_Path = "{}/Orientation.txt".format(Abaqus_Temp_Files_Path)
-    Load_Evaluation = []
-
-    for load in Load_Case:
+    for load in load_case:
         if load > 0:
-            Load_Evaluation.append(1)
+            load_evaluation.append(1)
         if load == 0:
-            Load_Evaluation.append(0)
+            load_evaluation.append(0)
         if load < 0:
-            Load_Evaluation.append(2)
+            load_evaluation.append(2)
 
-    Load_String = ''.join(str(e) for e in Load_Case)
-    Load_Hash = hashlib.sha256(Load_String.encode('utf-8')).hexdigest()
-    Tx="Rnd"
-    with open(Orientation_File_Path) as f:
+    load_string = ''.join(str(e) for e in load_case)
+    load_hash = hashlib.sha256(load_string.encode('utf-8')).hexdigest()
+    # TODO: It is pretty hard to assign a certain name for every texture. So the tx in the key may be not so informative.
+    tx = "Rnd"
+    with open(orientation_file_path) as f:
         data = f.read()
-        Orientation_Hash = hashlib.sha256(data.encode('utf-8')).hexdigest()
-    Key = "Us_A{}B{}C{}D{}E{}F{}_{}_{}_Tx_{}".format(Load_Evaluation[0],Load_Evaluation[1],Load_Evaluation[2],Load_Evaluation[3],Load_Evaluation[4],Load_Evaluation[5],Load_Hash[:5],Orientation_Hash[:5],Tx)
-    return (Key)
+        orientation_hash = hashlib.sha256(data.encode('utf-8')).hexdigest()
 
+    if ori_file != 'Orientations.txt':
+        try:
+            # e.g. orientations_ori-hash.txt
+            ori_hash = ori_file.split('_')[1].split('.')[0]
+        except ValueError:
+            print('ori_file {} not in standard format orientations_ori-hash.txt'.format(ori_file))
 
+        if ori_hash != orientation_hash[:5]:
+            # TODO warning string is not  working with formating.
+            warnings.warn('Orientation file %s was given. Hash in Filename %s is NOT consistent with '
+                              'generated hash for key %s.' % (ori_file, ori_hash, orientation_hash))
+
+    key = "Us_A{}B{}C{}D{}E{}F{}_{}_{}_{}_{}_Tx_{}".format(load_evaluation[0], load_evaluation[1], load_evaluation[2],
+                                                           load_evaluation[3], load_evaluation[4], load_evaluation[5],
+                                                           load_hash[:5], n_grains_per_dir ** 3, elements_per_grain,
+                                                           orientation_hash[:5], tx)
+    return key
