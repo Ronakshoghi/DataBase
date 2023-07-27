4b12b18beda6606b3609faf596a54b7bf87435da
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2023-07-27 11:54:08 -0400

    Changed to Python style guide conventions (only lower cae letters for functions) and added openphase input generator.

--- src/Geom_Generator.py
+++ src/Geom_Generator.py
@@ -16,10 +16,11 @@ for this code the geometry file and the remPart should be available in the key f
 import os
 import shutil
 
-def Geom_Input_Generator(Abaqus_Temp_Files_Path):
-    Geometry_File_Path = "{}/geometry_Periodic.inp".format(Abaqus_Temp_Files_Path)
-    with open(Geometry_File_Path, "r") as fr:
-        material_addition = ['**\n', '*Include, input='+"Material.inp"+'\n']
+
+def geom_input_generator(abaqus_temp_files_path):
+    geometry_file_path = "{}/geometry_Periodic.inp".format(abaqus_temp_files_path)
+    with open(geometry_file_path, "r") as fr:
+        material_addition = ['**\n', '*Include, input=' + "Material.inp" + '\n']
         prev_line = False
         lines = []
         for line in fr:
@@ -27,49 +28,135 @@ def Geom_Input_Generator(Abaqus_Temp_Files_Path):
                 lines.append(line)
             if '*End Assembly' in line:
                 prev_line = True
-    with open(Geometry_File_Path, 'w') as fr:
+    with open(geometry_file_path, 'w') as fr:
         for line in lines:
             fr.write(line)
         fr.writelines(material_addition)
     return None
 
+
 # A function to generate the geometry input file for each key, look for the key folder and write the geom file in that.
 # The name also changes for each key
-# Abaqus_Temp_Files_Path = "{}/Abaqus_Temp_Files".format(Current_Path)
+# abaqus_temp_files_path = "{}/Abaqus_Temp_Files".format(current_path)
 
 
-def Abaqus_Input_Generator(Key):
-    Current_Path = os.getcwd()
-    Abaqus_Temp_Files_Path = "{}/Abaqus_Temp_Files".format(Current_Path)
-    Geom_Input_Generator(Abaqus_Temp_Files_Path)
-    Geometry_File_Path = "{}/geometry_Periodic.inp".format(Abaqus_Temp_Files_Path)
-    Material_File_Path = "{}/Material.inp".format(Abaqus_Temp_Files_Path)
-    Orientation_File_Path = "{}/Orientation.txt".format(Abaqus_Temp_Files_Path)
-    Keys_Path ="{}/Keys".format(Current_Path)
-    os.chdir(Keys_Path)
-    Key_path = os.path.abspath(Key)
-    Key_Inputs_Path = "{}/inputs".format(Key_path)
-    files_path = [Geometry_File_Path, Material_File_Path, Orientation_File_Path]
+def abaqus_input_generator(Key):
+    current_path = os.getcwd()
+    abaqus_temp_files_path = "{}/Abaqus_Temp_Files".format(current_path)
+    geom_input_generator(abaqus_temp_files_path)
+    geometry_file_path = "{}/geometry_Periodic.inp".format(abaqus_temp_files_path)
+    material_file_path = "{}/Material.inp".format(abaqus_temp_files_path)
+    orientation_file_path = "{}/Orientation.txt".format(abaqus_temp_files_path)
+    keys_path = "{}/Keys".format(current_path)
+    os.chdir(keys_path)
+    key_path = os.path.abspath(Key)
+    key_inputs_path = "{}/inputs".format(key_path)
+    files_path = [geometry_file_path, material_file_path, orientation_file_path]
     for file in files_path:
-        shutil.copy2(file, Key_Inputs_Path)
+        shutil.copy2(file, key_inputs_path)
     result = []
-    for root, dir, files in os.walk(Key_Inputs_Path):
+    for root, dir, files in os.walk(key_inputs_path):
         for filename in files:
             if "remPart" in filename:
                 result.append(os.path.join(root, filename))
-    Load_File_Path = result[0]
-    Load_File_Name = os.path.basename(Load_File_Path)
-    Geom_File_Path = "{}/geometry_Periodic.inp".format(Key_Inputs_Path)
-    with open(Geom_File_Path, "a") as fr:
-         load_addition = ['**\n', '*Include, input='+Load_File_Name+'\n','**\n']
-         fr.writelines(load_addition)
+    load_file_path = result[0]
+    load_file_name = os.path.basename(load_file_path)
+    geom_file_path = "{}/geometry_Periodic.inp".format(key_inputs_path)
+    with open(geom_file_path, "a") as fr:
+        load_addition = ['**\n', '*Include, input=' + load_file_name + '\n', '**\n']
+        fr.writelines(load_addition)
     fr.close()
-    Geom_File_Name_New = Key + "_Abaqus_Input_File.inp"
-    os.chdir(Key_Inputs_Path)
-    Geom_File_Name_New_path = os.path.abspath(Geom_File_Name_New)
-    os.rename(Geom_File_Path, Geom_File_Name_New_path)
-    os.chdir(Current_Path)
+    geom_file_name_new = Key + "_Abaqus_Input_File.inp"
+    os.chdir(key_inputs_path)
+    geom_file_name_new_path = os.path.abspath(geom_file_name_new)
+    os.rename(geom_file_path, geom_file_name_new_path)
+    os.chdir(current_path)
 
 
+def openphase_input_generator(load, key, n_grains_per_dir, elements_per_grain, ori_file='Orientation.txt'):
+    """
+    This function modifies the ProjectInput.opi and the Matchbox.cpp. Currently, loads are changed according to load
+    variable, discretization is changed according to n_grains and elments_per_grain.
+    :param elements_per_grain:
+    :param n_grains_per_dir:
+    :param load:
+    :param key:
+    :return:
+    """
 
+    current_path = os.getcwd()
+    temp_files_path = "{}/OpenPhase_Temp_Files".format(current_path)
+    input_file = "{}/ProjectInput.opi".format(temp_files_path)
+    cpp_file = "{}/MatchBox.cpp".format(temp_files_path)
+    make_file = "{}/Makefile".format(temp_files_path)
+
+
+    # Modify ProjectInput.opi
+    keys_path = "{}/Keys".format(current_path)
+    os.chdir(keys_path)
+    key_path = os.path.abspath(key)
+    key_inputs_path = "{}/inputs".format(key_path)
+    os.chdir(key_inputs_path)
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+    keywords_bc_types = ['$BCX', '$BCY', '$BCZ', '$BCYZ', '$BCXZ', '$BCXY']
+    keywords_bc_values = ['$BCValueX', '$BCValueY', '$BCValueZ', '$BCValueYZ', '$BCValueXZ', '$BCValueXY']
+    load_dict = {}
+    for idx_comp, component in enumerate(load):
+        if component < -1e-5 or component > 1e-5:
+            load_dict[idx_comp] = {'type': "AppliedStress", 'value': component}
+        else:
+            load_dict[idx_comp] = {'type': "FreeBoundaries"}
+    with open("{}.opi".format(key), 'w+') as f:
+        for line in lines:
+            if '$SimTtl' in line:
+                f.write('$SimTtl         Simulation Title                  : {}\n'.format(key))
+            elif 'Type of boundary conditions:' in line:
+                for idx_dir, bc_dict in load_dict.items():
+                    f.write('{bckey}	Boundary condition in {dir} direction :  {value}\n'.format(
+                        bckey=keywords_bc_types[idx_dir], dir=keywords_bc_types[idx_dir].split('C')[-1],
+                        value=bc_dict['type']))
+
+            elif 'Values of boundary conditions:' in line:
+                # f.write('Values of boundary conditions: \n')
+                for idx_dir, bc_dict in load_dict.items():
+                    if 'value' in bc_dict.keys():
+                        f.write(
+                            '{bckey}    Mixed Value {dir}   : {value}\n'.format(bckey=keywords_bc_values[idx_dir],
+                                                                                dir=
+                                                                                keywords_bc_types[idx_dir].split(
+                                                                                    'C')[-1],
+                                                                                value=bc_dict['value']))
+            elif '$Nx' in line:
+                f.write('$Nx             System Size in X Direction              : {}\n'.format(n_grains_per_dir))
+            elif '$Ny' in line:
+                f.write('$Ny             System Size in Y Direction              : {}\n'.format(n_grains_per_dir))
+            elif '$Nz' in line:
+                f.write('$Nz             System Size in Z Direction              : {}\n'.format(n_grains_per_dir))
+            else:
+                f.write(line)
+        f.close()
+
+    # Modify the Matchbox.cpp
+    with open(cpp_file, 'r') as f:
+        lines = f.readlines()
+
+    with open("MatchBox.cpp".format(key), 'w+') as f:
+        for line in lines:
+            if 'string OutFile' in line:
+                f.write('    string OutFile       = OPSettings.TextDir +"{key}.txt";\n'.format(key=key))
+            elif 'Initializations::MatchBox' in line:
+                f.write('        	  Initializations::MatchBox(Phi, BC, OPSettings, {epg});\n'.format(
+                    epg=int(elements_per_grain**(1/3))))
+            elif 'OR.SetGrainOrientationsFromFile' in line:
+                f.write('            OR.SetGrainOrientationsFromFile(Phi, "{orifile}");\n'.format(orifile=ori_file))
+            else:
+                f.write(line)
+        f.close()
+
+    # Copy the Makefile and the orientation file
+    shutil.copy2(make_file, "Makefile")
+    shutil.copy(os.path.join(temp_files_path, ori_file), ori_file)
+
+    os.chdir(current_path)
 
