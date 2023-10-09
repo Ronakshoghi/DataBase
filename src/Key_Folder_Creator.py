1ff0039dc0472f38c5cb1667cc6b8b7c94cfbd58
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2023-10-09 17:56:05 -0400

    When creating Key on /scratch, and key subdirectories (= rundirs), create a oreintaiton.csv based on texure_file and copy to key_subdir.

--- src/Key_Folder_Creator.py
+++ src/Key_Folder_Creator.py
@@ -9,6 +9,9 @@ This function creates a local folder with the specified path for each key.
 import os
 import shutil
 
+import numpy as np
+import json
+
 
 def create_key_folder(keys_folder_name, cp):
     keys_path = "{}/Keys".format(cp)
@@ -20,19 +23,24 @@ def create_key_folder(keys_folder_name, cp):
             os.makedirs(keys_folder_name)
     except OSError:
         print('Error: Creating directory {}.'.format(keys_folder_name))
+    return os.path.join(keys_path, keys_folder_name)
 
 
 # a function to create 2 subfolder of inputs and results for each key and copy the constant abaqus file into the input.
 
-def create_sub_folder(key, cp_code='abaqus'):
+def create_sub_folder(key, cp_code='abaqus', ori_file=None):
     current_path = os.getcwd()
     if cp_code == 'abaqus':
         constant_files_path = "{}/Abaqus_Constant_Files".format(current_path)
     elif cp_code == 'openphase':
         constant_files_path = "{}/OpenPhase_Constant_Files".format(current_path)
+        if not ori_file:
+            raise ValueError("OpenPhase requires the absolute path of the texture file.")
     else:
         raise ValueError("cp_code {code} not valid. Must be abaqus or openphase.".format(code=cp_code))
-    create_key_folder(key, current_path)
+
+    # Create the run directory for the key
+    run_dir = create_key_folder(key, current_path)
 
     if cp_code == 'abaqus':
         folders = ['inputs', 'results']
@@ -42,7 +50,7 @@ def create_sub_folder(key, cp_code='abaqus'):
                     os.makedirs(os.path.join(key, folder))
             except OSError:
                 print('Error: Creating directory. ' + os.path.join(key, folder))
-        current_path_1 = os.path.abspath(key)
+        current_path_1 = os.path.abspath(key) # this is equal to rundir
         key_inputs_path = "{}/inputs".format(current_path_1)
         for file_name in os.listdir(constant_files_path):
             source = os.path.join(constant_files_path, file_name)
@@ -50,7 +58,12 @@ def create_sub_folder(key, cp_code='abaqus'):
             if os.path.isfile(source):
                 shutil.copy(source, destination)
 
-    elif cp_code =='openphase':
-        print('Current state: no constant files, all temporary. I do not create subfolders "input" and "results".')
-    os.chdir(current_path)
+    elif cp_code == 'openphase':
+        # In the cluster version create a orientation.csv file in the key directory
+        with open(ori_file, 'r') as f:
+            data_json = json.load(f)
+        data_ori = np.array(data_json['discrete_orientations'])
+        ori_file_name = key + "_orientations.csv"
+        np.savetxt(os.path.join(run_dir, ori_file_name), data_ori, delimiter=",")
 
+    os.chdir(current_path)
