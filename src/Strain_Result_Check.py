dea12da15aab2097138338210728a79c570a1b14
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2024-11-04 13:30:18 +0100

    Added docstring.

--- src/Strain_Result_Check.py
+++ src/Strain_Result_Check.py
@@ -10,6 +10,19 @@ from numpy import genfromtxt
 
 
 def max_strain_finder(key, cp_code):
+    """
+    Finds the equivalent strain of the last increment in the result file.
+    Parameters
+    ----------
+    key : str
+        BC Key of format Us_A{}B{}C{}D{}E{}F{}_{load_hash}_{n_grains}_{elements_per_grain}_{ori_hash}_Tx_{tx_type}
+    cp_code : str
+        CP code used. Either abaqus or openphase.
+
+    Returns
+    -------
+    maximum equivalent total strain.
+    """
     current_path = os.getcwd()
     keys_path = "{}/Keys".format(current_path)
     os.chdir(keys_path)
@@ -18,9 +31,24 @@ def max_strain_finder(key, cp_code):
     os.chdir(key_results_path)
     if cp_code == 'abaqus':
         strains = genfromtxt('E.out', delimiter=' ')
-    elif cp_code =='openphase':
-        strains = genfromtxt('{}.txt'.format(key), delimiter=';', skip_header=1)[:, -2]
+    elif cp_code == 'openphase':
+        try:
+            strains = genfromtxt('{}.txt'.format(key), delimiter=';', skip_header=1)[:, -2]
+        except IndexError:
+            strains = [genfromtxt('{}.txt'.format(key), delimiter=';', skip_header=1)[-2]]
     else:
         raise ValueError("cp_code {code} not valid. Must be abaqus or openphase.".format(code=cp_code))
     os.chdir(current_path)
     return max(strains)
+
+
+def count_converged_steps(key):
+    current_path = os.getcwd()
+    keys_path = "{}/Keys".format(current_path)
+    os.chdir(keys_path)
+    key_path = os.path.abspath(key)
+    key_results_path = "{}/results".format(key_path)
+    os.chdir(key_results_path)
+    strains = genfromtxt('{}.txt'.format(key), delimiter=';', skip_header=1)
+    os.chdir(current_path)
+    return len(strains)
