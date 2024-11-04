50a54ee7dadb3a8d7d9b70521ed2c863077e9418
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2024-11-04 12:16:18 +0100

    Added Docstring to openphase_runner

--- src/CP_Runner.py
+++ src/CP_Runner.py
@@ -4,6 +4,9 @@ Author: Ronak Shoghi
 Date: 23.05.22
 Time: 11:23
 
+Modified: Jan Schmidt
+Date: 04.11.24
+
 Abaqus runner scrip for each key using the prepared input files
 """
 
@@ -31,13 +34,30 @@ def Abaqus_Runner(Key, ncpu):
 
 
 def openphase_runner(key, t_timeout=300, log_files_dir=None):
+    """
+    Runs the OpenPhase simulation by using the subprocess package.
+    First, the updated C++ file is compiled via make command. Afterwards, the executable is run.
+
+    Parameters
+    ----------
+    key : str
+        BC Key of format Us_A{}B{}C{}D{}E{}F{}_{load_hash}_{n_grains}_{elements_per_grain}_{ori_hash}_Tx_{tx_type}
+    t_timeout : double
+        Seconds after which the subprocess that triggers OpenPhase simulation is timed-out.
+    log_files_dir : str
+        Directory to the LogFiles.txt.
+
+    Returns
+    -------
+
+    """
     print(key)
     if not log_files_dir:
         log_files_dir = os.getcwd()
-    current_path = os.getcwd()  # /scratch
-    simu_path = os.path.join(current_path, "Keys/{}".format(key))
-    command_make = "make SETTINGS=static"
-    command_run = "./Matchbox {inputfile}".format(inputfile=key + ".opi")
+    current_path = os.getcwd()  # /{your_RVE_dict}
+    simu_path = os.path.join(current_path, "Keys/{}".format(key)) #sub-dir in which the OpenPhase simulation will run
+    command_make = "make SETTINGS=static" # static make. Might be changed if dynamic libraries are compiled.
+    command_run = "./MatchBox {inputfile}".format(inputfile=key + ".opi")
 
     # Make
     try:
@@ -51,12 +71,12 @@ def openphase_runner(key, t_timeout=300, log_files_dir=None):
     try:
         filename = os.path.join(log_files_dir, "{}.log".format(key))
         print(filename)
-        with open(filename, "w") as f:
-            output = subprocess.run(command_run, cwd=simu_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+        #with open(filename, "w") as f:
+        output = subprocess.run(command_run, cwd=simu_path, stdout=open(filename, 'w'), stderr=subprocess.STDOUT,
                                     check=True, shell=True, timeout=t_timeout, text=True)
 
-            for line in output.stdout:
-                f.write(line)
+        #    for line in output.stdout:
+        #        f.write(line)
         # Remove log file if simulation converges
         os.remove(filename)
 
