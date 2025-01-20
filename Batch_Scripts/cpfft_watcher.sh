2bf01e0127355efdc865c68cd2d676618ed7e745
smidtjzh <jan.schmidt-p2d@ruhr-uni-bochum.de>
2025-01-20 14:06:35 +0100

    single run to check r-values

--- Batch_Scripts/cpfft_watcher.sh
+++ Batch_Scripts/cpfft_watcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nit=15  # how often should this script be run (15)
-nrun=10  # counts iterations of this script
-ntex=500  # number of textures per iteration
+nit=1  # how often should this script be run (15)
+nrun=0  # counts iterations of this script
+ntex=1  # number of textures per iteration
 
 module load anaconda3/
 conda activate data_generation
@@ -16,10 +16,10 @@ do
     echo "$njobs"
     sleep 30
   done
-  python create_dataset_remote.py -name 3DBC_CPFFT -nt $ntex \
+  python create_dataset_remote.py -name RValues -nt $ntex \
   -ids $idx_start  \
-  -dbp /storage/home/hcoda1/7/jschmidt87/scratch/3DBC_CPFFT \
-  -sp /storage/home/hcoda1/7/jschmidt87/scratch/3DBC_CPFFT \
+  -dbp /storage/home/hcoda1/7/jschmidt87/scratch/RValues \
+  -sp /storage/home/hcoda1/7/jschmidt87/scratch/RValues \
   -n_gpd 11 -n_epg 1 -t_to 300
   echo "I initiated $ntex jobs starting from $idx_start"
   #echo "index start is $idx_start" 
